f54ff9e65ad278b9ad066bc4bc67d1b34387498f
mdekabs <1mawoda@gmail.com>
2023-11-23 08:40:31 +0100

    config file for the openai

---
+++ config/open_ai.js
@@ -0,0 +1,11 @@
+/* importing neccessary modules */
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv"
+dotenv.config();
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+export default openai;
