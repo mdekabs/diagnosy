730083ba4cc9a0cb735a312511c0f4d0759e1124
mdekabs <1mawoda@gmail.com>
2023-11-24 15:44:31 +0100

    CHATBOT CONTROLLER IN THE MAKING

--- config/open_ai.js
+++
@@ -1,9 +0,0 @@
-/* importing neccessary modules */
-import OpenAI from "openai";
-import dotenv from "dotenv"
-dotenv.config();
-
-const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
-});
-export default openai;
