b348e2c8e074645495d39bdaadc70e1b7be5e682
mdekabs <1mawoda@gmail.com>
2023-11-25 15:03:33 +0100

    chatcontroller

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -7,35 +7,46 @@ class ChatController {
 	static async createChat(request, response) {
 		// console.log("here")
 		const {message, userID} = request.body;
-		if (!userID) {
-			response.status(400).json({
-				status: "error",
-				message: "User ID is required",
-				data: null,
-			  })
+		try {
+			/* i want to validate user and get chat history */
+			const chatHistory = await createChatHistory(userID, message);
+			const aiResponse = await OpenaiService.getChatbotCompletion(chatHistory);
+			chatHistory.push(["assistant", aiResponse]);
+			await dbClient.saveChatHistory(userID, chatHistory);
+			response.status(200).json({
+				status: "successful",
+				message: "chat is created and saved",
+				data: { chatHistory, aiResponse },
+			});
 		}
-		const user = dbClient.fetchUserByID(userID);
-		if (!user) {
-			response.status(404).json({
-				status: "error",
-				message: "User not found",
-				data: null,
-			  });
-		}
-		console.log(createChatHistory(userID), message)
-		// const advice = OpenaiService.getChatbotCompletion()
+		catch (error) {
+			console.error("error creatin chat:", error);
+			response.status(500);
+		};
 	}
+	static async createChatHistory(userID, userMessage) {
+		try {
+			/* fetch user */
+			const user = await dbclient.fetchUserByID(userID);
+			if (!user) {
+				throw new Error("user not found");
+			}
+			/* initialize a chat or get existing chat history */
+			let chatHistory = user.chatHistory || [];
+			const systemMessage = 
+				"Your name is Daisy. You are a Symptom and Diagnosis Guidance bot. You provide preliminary medical diagnoses and advice to patients based on their symptoms and help them schedule an appointment with a medical professional. If needed, I can help you schedule an appointment with a medical practitioner. Would you like assistance with that?";
+			if (chatHistory.length === 0) {
+				chatHistory.push(["system", systemMessage]);
+			}
 
-	static async createChatHistory(userID) {
-		const chatHistory = [];
-		const systemMessage = "Your name is Daisy. You are a Symptom and Diagnosis Guidance bot. You provide preliminary medical diagnoses and advice to patients based on their symptoms and help them schedule an appointment with a medical professional. If needed, I can help you schedule an appointment with a medical practitioner. Would you like assistance with that";
-		chatHistory.push(["system", systemMessage]);
-		const chat = {chatHistory, userID}
-
-		return await dbClient.createChat(chat);
-	  }
-
-	
+			/* add user message to chat history */
+			chatHistory.push(["user", userMessage]);
+			return chatHistory;
+		}
+		catch (error) {
+			console.error("error creating chathistory:", error);
+			return error;
+		}
+	}
 }
-
 export default ChatController;
