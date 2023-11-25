09a77152874b38cdc4dccea138160645f6a6ed73
MikeRock51 <mikerockmusic51@gmail.com>
2023-11-25 09:07:24 +0100

    Improve auth route payload

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -19,40 +19,65 @@ class AuthController {
           console.log(reply);
           response
             .status(200)
-            .json({ status: "success", message: "Sign in successful", token })
+            .json({
+              status: "success",
+              message: "Sign in successful",
+              data: { token },
+            })
             .end();
         } catch (error) {
           console.error(error);
           response
-            .status(501)
-            .json({ status: "error", message: error.message })
+            .status(504)
+            .json({ status: "error", message: error.message, data: null })
             .end();
         }
       } else {
         response
           .status(401)
-          .json({ status: "error", message: "Unauthorized" })
+          .json({
+            status: "error",
+            message: "Incorrect password",
+            data: null,
+          })
           .end();
       }
     } else {
       response
         .status(401)
-        .json({ status: "error", message: "Unauthorized" })
+        .json({
+          status: "error",
+          message: "User does not exist",
+          data: null,
+        })
         .end();
     }
   }
 
   static async getDisconnect(request, response) {
-    const token = request.headers["x-token"];
+    const token = request.headers["auth-token"];
     const key = `auth_${token}`;
     const userID = await redisClient.get(key);
     if (!userID) {
-      response.status(401).json({ error: "Unauthorized" }).end();
+      response
+        .status(401)
+        .json({
+          status: "error",
+          message: "Invalid token",
+          data: null,
+        })
+        .end();
     } else {
       const user = await dbClient.fetchUserByID(userID);
-
       if (!user) {
-        response.status(401).json({ error: "Unauthorized" }).end();
+        response
+          .status(404)
+          .json({
+            status: "error",
+            message: "Invalid token",
+            data: null,
+          })
+          .end();
       } else {
         const reply = await redisClient.del(key);
         console.log(reply);
@@ -62,27 +87,41 @@ class AuthController {
   }
 
   static async getMe(request, response) {
-    const token = request.headers["x-token"];
+    const token = request.headers["auth-token"];
     const key = `auth_${token}`;
     try {
       const userID = await redisClient.get(key);
       if (!userID) {
-        response.status(401).json({ error: "Unauthorized" }).end();
+        response
+          .status(401)
+          .json({
+            status: "error",
+            message: "Invalid token",
+            data: null,
+          })
+          .end();
       } else {
         const user = await dbClient.fetchUserByID(userID);
-
         if (!user) {
-          response.status(401).json({ error: "Unauthorized" }).end();
+          response
+            .status(404)
+            .json({
+              status: "error",
+              message: "User not found",
+              data: null,
+            })
+            .end();
         } else {
           response
             .status(200)
             .json({
               status: "success",
+              message: "Current user fetched successfully",
               data: {
                 id: user._id,
                 email: user.email,
                 username: user.username,
-              }
+              },
             })
             .end();
         }
