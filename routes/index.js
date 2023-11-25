aa5bf3544e95310366b8ee450d8f057754292fdb
mdekabs <1mawoda@gmail.com>
2023-11-25 22:39:06 +0100

    correction

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,6 @@ router.post('/users', UsersController.postNew);
 router.get('/sign_in', AuthController.getConnect);
 router.get('/sign_out', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
-router.get("/chats", ChatController.createChat);
+router.get("/chats", ChatController.handleChat);
 
 export default router;
