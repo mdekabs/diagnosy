b480fba591e2aa68af524a423f65426d000ef9f1
mdekabs <1mawoda@gmail.com>
2023-11-26 09:04:55 +0100

    route worked on

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.post('/users', UsersController.postNew);
 router.get('/sign_in', AuthController.getConnect);
 router.get('/sign_out', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
-router.get("/chat", ChatController.handleChat);
+router.get("/chat", async (req, res) => {
+  const chatController = new ChatController();
+  await chatController.handleChat();
+});
 
 export default router;
