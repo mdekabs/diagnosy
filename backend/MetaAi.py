9b3575d1ecd0f6e88c552dfc3d968818f0f790e4
mdekabs <1mawoda@gmail.com>
2024-04-20 22:19:51 +0100

    generate the metaAi

---
+++ backend/MetaAi.py
@@ -0,0 +1,5 @@
+from meta_ai_api import MetaAI
+
+def get_meta_ai_instance():
+    ai = MetaAI()
+    return ai
--- backend/storage/redis.js
+++ backend/storage/redis.js
@@ -102,4 +102,3 @@ class RedisClient {
 const redisClient = new RedisClient();
 
 export default redisClient;
-  
